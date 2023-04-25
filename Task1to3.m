087a34aeb2a9467a49009116816d4a80fe57fc3a
Jacob Morgan <jake2002.jm@gmail.com>
2023-04-25 21:20:22 +0100

    late addition

--- Task1to3.m
+++ Task1to3.m
@@ -1,62 +1,58 @@
-
-
-
-
-
-
-
-
-
-
 % MATLAB script for Assessment 1
 % Task 1: Preprocessing ---------------------------
 clear; close all; clc;
 
 % Step-1: Load input image
-I = imread('Images/IMG_01.JPG');
+I = imread('Assignment_Input/IMG_01.JPG');
 figure;
 imshow(I);
-
+title("Step-1: Load input image");
 
 % Step-2: Conversion of input image to grey-scale image
 Igray = rgb2gray(I);
 figure;
-imshow(Igray);
+imshow(Igray)
 size(Igray)
-
+title("Step-2: Conversion of input image to grey-scale image")
 
 % Step-3: Resizing the grayscale image using bilinear interpolation
-
-
 Igraybi = imresize(Igray,0.5,"bilinear");
 figure;
-imshow(Igraybi);
+imshow(Igraybi)
 size(Igraybi)
+title("Step-3: Resizing the grayscale image using bilinear interpolation")
 
 % Step-4: Generating histogram for the resized image
-
-histogram(Igraybi,20);
-
-%histogram drops off at around 150. so we do 150/255 = 0.5888
-
-
-threshold = double(150/255);
+figure;
+histogram(Igraybi,20)
+title("Step-4: Generating histogram for the resized image")
 
 % Step-5: Producing binarised image
+threshold = double(210/255);
 
-BW = imbinarize(Igraybi,threshold);
-imshow(BW);
-
-
+binarisedImage = imbinarize(Igraybi,threshold);
+figure;
+imshow(binarisedImage)
+title("Step-5: Producing binarised image")
 
 
 %---------------------------------------------------------------
 % Task 2: Edge Detection -----------------------
-
-
+edgeDetectionSobel = edge(Igraybi,'sobel');
+figure;
+imshow(edgeDetectionSobel)
+title("Task 2: Edge Detection - Sobel")
+edgeDetectionCanny = edge(Igraybi,'canny');
+figure; 
+imshow(edgeDetectionCanny)
+title("Task 2: Edge Detection - Canny")
 
 %---------------------------------------------------------------
 % Task-3: Simple Segmentation -----------------------
-
+binerisedImagecleaner = bwareafilt(binarisedImage,[2000,7500]); %removes the biggest section on the right
+figure;
+imshow(binerisedImagecleaner)
+title("Task 3: Simple Segmentation")
+imwrite(binerisedImagecleaner,"Assignment_Input/binswan.jpg")
 
 
--- Task4to5.m
+++ Task4to5.m
@@ -1,12 +1,63 @@
 % MATLAB script for Assessment Item-1
-% Task 4: Robust Swan Recognition ----------------
 clear; close all; clc;
+GT = imread("Assignment_Input/binswan.jpg");
+GT = imcrop(GT,[440.5 340.5 249 223]);
+
+dice_scores = [];
+
+for i = 01:16 
+  if i < 10
+    i = sprintf("%02d",i);
+  end 
+  
+  try
+    groundtruth = imread("Assignment_GT/IMG_" + i + "_GT.JPG");
+
+    actual = imread("Assignment_Input/IMG_" + i + ".JPG");
+
+    Ir = swan_detection(actual, GT);
+    figure;
+    imshow(Ir);
+    title("Swan " +i)
+    Ir = imbinarize(Ir,0.9);
+    groundtruth = imbinarize(groundtruth,0.9);
+    dice_score = dice(Ir, groundtruth);
+    dice_scores = [dice_scores dice_score];
+  catch ME
+    disp("Error Threshold too low")
+  end
+end
+
+meandice = mean(dice_scores)
+stddice  = std(dice_scores)
+dice_scores
+
+function Ir = swan_detection(actual,GT)
+
+  actualGray = rgb2gray(actual);
+
+  swanPoints = detectSURFFeatures(GT);
+
+  meanContrast = mean(mean(actualGray));
+  img = imadjust(actualGray,[meanContrast/255,1]);
+  thresh = graythresh(img);
+
+  binarisedImage = imbinarize(actualGray,0.7);
+
+  actualPoints = detectSURFFeatures(binarisedImage);
+
+  [sceneFeatures,scenePoints] = extractFeatures(binarisedImage,actualPoints);
 
 
-% Task 5: Performance Evaluation ----------------- 
-% Step 1: Load ground truth data
-GT = imread("IMG_01_GT.png");
+  [swanFeatures,swanPoints] = extractFeatures(GT,swanPoints);
+  swanPairs = matchFeatures(swanFeatures,sceneFeatures);
+  matchedSwanPoints = swanPoints(swanPairs(:,1),:);
+  matchedScenePoints = scenePoints(swanPairs(:,2),:);
 
-% To visualise the ground truth image, you can
-% use the following code.
-figure, imshow(GT)
+  [tform, inlierSwanPoints, inlierScenePoints] = estimateGeometricTransform(matchedSwanPoints, matchedScenePoints, 'affine');
+
+  % Transform obj image onto input image  % On the obj image variable to get output 
+  outputView = imref2d(size(binarisedImage)); 
+  Ir = imwarp(GT,tform,"OutputView",outputView);
+
+end
